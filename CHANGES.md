77af2fda98e46b2605f34e9b58b946f9594ecd60
Micah <micahmartin@gmail.com>
2014-02-20 11:31:35 -0600

    updates dependecies

---
+++ CHANGES.md
@@ -0,0 +1,8 @@
+# 1.1.0
+
+* removes dependency on hyperion
+* updates dependencies
+
+# 1.0.0
+
+* initial release
--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject com.slagyr/blobstore "1.0.0"
+(defproject com.slagyr/blobstore "1.1.0"
   :description "API for storing blobs of data"
   :url ""
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[clj-aws-s3 "0.3.7"]
+  :dependencies [[clj-aws-s3 "0.3.8"]
                  [com.taoensso/timbre "3.0.1"]
-                 [org.clojure/clojure "1.5.0"]]
-  :profiles {:dev {:dependencies [[speclj "2.7.4"]]}}
-  :plugins [[speclj "2.7.4"]]
+                 [org.clojure/clojure "1.5.1"]]
+  :profiles {:dev {:dependencies [[speclj "2.9.1"]]}}
+  :plugins [[speclj "2.9.1"]]
   :test-paths ["spec"])
